df63dab93bb4b3c132cfcc3c35b3feba24d33bea
Sean Sanders <leapfrogopus@yahoo.com>
2022-06-23 09:17:31 -0400

    Adding shell script

---
+++ Presentations/Activity/Script.sh
@@ -0,0 +1,12 @@
+mv Ant-Man ~/Desktop/
+mv Aquaman ~/Desktop/
+
+mv Asterix ~/Documents/
+mv 'The Atom' ~/Documents/
+
+mv 'The Avengers' ~/Downloads/
+mv Batgirl ~/Downloads/
+
+mv 'Black Canary' ~/Public/
+mv 'Black Panther' ~/Public/
+
